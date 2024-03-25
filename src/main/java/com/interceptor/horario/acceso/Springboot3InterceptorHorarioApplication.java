6dadf50b051e9df12e1475c9b006813532ac3871
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-03-25 13:15:17 -0500

    Creacion de Interceptor HTTP por Horario

---
+++ src/main/java/com/interceptor/horario/acceso/Springboot3InterceptorHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.interceptor.horario.acceso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3InterceptorHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3InterceptorHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interceptor/horario/acceso/Springboot3InterceptorHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interceptor.horario.acceso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3InterceptorHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
